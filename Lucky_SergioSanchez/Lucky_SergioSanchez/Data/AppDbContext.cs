b52a193a215f2ee589d729bf1f90c2c4dfc42efe
sergioasp12 <65974619+sergioasp12@users.noreply.github.com>
2021-10-13 19:28:00 -0400

    AppDbContext terminado

---
+++ Lucky_SergioSanchez/Lucky_SergioSanchez/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Lucky_SergioSanchez.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky_SergioSanchez.Data
+{
+    public class AppDbContext:DbContext
+    {
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options): base (options)
+        {
+
+        }
+        public DbSet<Lucky> Lucky { get; set; }
+    }
+}
